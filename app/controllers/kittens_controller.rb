a8421dc48e3d0b507dce48354752728f2d4bbe29
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-12-03 16:29:04 +0100

    Fix typo in kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -28,7 +28,7 @@ class KittensController < ApplicationController
 	def update
 		@kitten = Kitten.find(params[:id])
 
-		if @kitten.update(article_params)
+		if @kitten.update(kitten_params)
 			redirect_to @kitten
 		else
 			render :edit, status: :unprocessable_entity
